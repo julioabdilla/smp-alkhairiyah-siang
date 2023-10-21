a8f59925733367cdf88229abab6bc9d25b47a9c9
Dev <dev@Devs-MacBook-Pro.local>
2023-10-21 17:41:57 +0700

    fix z-index

--- src/components/photogrid/PhotoGrid.vue
+++ src/components/photogrid/PhotoGrid.vue
@@ -3,14 +3,14 @@
     <div v-for="(photo, index) in photos" :key="index" class="flex bg-gray-300 items-center">
       <img :src="photo.url" @click="openImage(photo.url)" alt="Photo" class="object-cover w-full aspect-video mx-auto cursor-pointer">
     </div>
-    <div v-if="selectedImage && detailPage" @click="closeImage" class="fixed top-0 left-0 bg-black bg-opacity-80 w-full h-full flex items-center justify-center">
+    <div v-if="selectedImage && detailPage" @click="closeImage" class="z-20 fixed top-0 left-0 bg-black bg-opacity-80 w-full h-full flex items-center justify-center">
       <div>
         <div class="bg-white bg-opacity-30 rounded-full mb-2 ml-auto w-8 p-1 cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" @click="closeImage" class="w-6 h-6">
             <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clip-rule="evenodd" />
           </svg>
         </div>
-        <img :src="selectedImage" alt="Full Image" class="max-h-full max-w-full w-[50em] transform transition-transform hover:scale-125" />
+        <img :src="selectedImage" alt="Full Image" class="max-h-full max-w-full w-[50em] md:transform md:transition-transform hover:scale-125" />
       </div>
     </div>
   </div>
